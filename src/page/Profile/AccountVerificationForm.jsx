cf18ac5f8b9316e5d0e2f73fd94fa8d2b40d8b44
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-01-17 21:34:05 +0000

    finished profile page

--- src/page/Profile/AccountVerificationForm.jsx
+++ src/page/Profile/AccountVerificationForm.jsx
@@ -1,40 +1,39 @@
-import { Button } from '@/components/ui/button'
-import React from 'react'
-import { Dialog, DialogTrigger, DialogHeader, DialogContent, DialogTitle } from '@/components/ui/dialog';
-import {InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator} from '@/components/ui/input-otp'
+import { Button } from '@/components/ui/button';
+import React, { useState } from 'react';
+import { InputOTP, InputOTPGroup, InputOTPSlot, InputOTPSeparator } from '@/components/ui/input-otp';
+import { DialogClose } from '@/components/ui/dialog';
 
 const AccountVerificationForm = ({ handleSubmit }) => {
+  const [value, setValue] = useState('');
+
   return (
     <div className="flex justify-center">
       <div className="space-y-5 mt-10 w-full">
-        <div className="flex justify-between items-center">
-          <p>Email: </p>
-          <p>TomsTrading@email.com</p>
-          <Dialog>
-            <DialogTrigger>
-              <Button onClick={handleSubmit}>Send OTP</Button>
-            </DialogTrigger>
-            <DialogContent>
-              <DialogHeader>
-                <DialogTitle>Enter OTP</DialogTitle>
-              </DialogHeader>
-              <div className="py-5 flex gap-10 justify-center items-center">
-                <InputOTP maxLength={6}>
-                  <InputOTPGroup>
-                    <InputOTPSlot index={0} />
-                    <InputOTPSlot index={1} />
-                    <InputOTPSlot index={2} />
-                  </InputOTPGroup>
-                  <InputOTPSeparator />
-                  <InputOTPGroup>
-                    <InputOTPSlot index={3} />
-                    <InputOTPSlot index={4} />
-                    <InputOTPSlot index={5} />
-                  </InputOTPGroup>
-                </InputOTP>
-              </div>
-            </DialogContent>
-          </Dialog>
+        <div className="flex flex-col items-center">
+          <div className="py-5 flex gap-10 justify-center items-center">
+            <InputOTP
+              value={value}
+              onChange={(value) => setValue(value)}
+              maxLength={6}
+            >
+              <InputOTPGroup>
+                <InputOTPSlot index={0} />
+                <InputOTPSlot index={1} />
+                <InputOTPSlot index={2} />
+              </InputOTPGroup>
+              <InputOTPSeparator />
+              <InputOTPGroup>
+                <InputOTPSlot index={3} />
+                <InputOTPSlot index={4} />
+                <InputOTPSlot index={5} />
+              </InputOTPGroup>
+            </InputOTP>
+            <DialogClose>
+              <Button onClick={() => handleSubmit(value)} className="w-full">
+                Submit
+              </Button>
+            </DialogClose>
+          </div>
         </div>
       </div>
     </div>
