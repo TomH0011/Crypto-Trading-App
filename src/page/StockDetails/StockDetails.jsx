976adaa553662a22f5c6ca895ea1b4f8f79efc1e
Tom Howard <165574283+TomH0011@users.noreply.github.com>
2025-01-17 21:59:12 +0000

    started further feature adding

--- src/page/StockDetails/StockDetails.jsx
+++ src/page/StockDetails/StockDetails.jsx
@@ -1,8 +1,20 @@
+import { Avatar, AvatarImage } from '@/components/ui/avatar'
 import React from 'react'
 
 const StockDetails = () => {
   return (
-    <div>StockDetails</div>
+    <div className='p-5 mt-5'>
+      <div className='flex justify-between'>
+        <div className='flex gap-5 justify-center'>
+
+          <Avatar>
+            <AvatarImage 
+            src = {'https://cryptologos.cc/logos/ethereum-eth-logo.png'}/>
+          </Avatar>
+
+        </div>
+      </div>
+    </div>
   )
 }
 
